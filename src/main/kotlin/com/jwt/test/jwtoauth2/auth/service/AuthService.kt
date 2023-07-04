368531c2306fa590c7d6f8046d88dd1ddfd0ce3c
wpghks <wpghks56@naver.com>
2023-07-04 18:00:51 +0900

    [feature] auth service 껍데기만

---
+++ src/main/kotlin/com/jwt/test/jwtoauth2/auth/service/AuthService.kt
@@ -0,0 +1,4 @@
+package com.jwt.test.jwtoauth2.auth.service
+
+interface AuthService {
+}
---
+++ src/main/kotlin/com/jwt/test/jwtoauth2/auth/service/impl/AuthServiceImpl.kt
@@ -0,0 +1,6 @@
+package com.jwt.test.jwtoauth2.auth.service.impl
+
+import com.jwt.test.jwtoauth2.auth.service.AuthService
+
+class AuthServiceImpl: AuthService {
+}
